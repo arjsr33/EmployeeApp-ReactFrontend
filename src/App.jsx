162efcb2ff09f3df3e408d798c45b9c2b8e2cb5a
arjsr33 <arjunsreedharan32@gmail.com>
2024-07-03 22:02:36 +0530

    basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
-import Home from './components/Home'
-import Form from './components/Form'
+import './App.css';
+import Home from './components/Home';
+import Form from './components/Form';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <Router basename="/EmployeeApp-ReactFrontend">
       <div>
         <Navbar />
         <Routes>
@@ -14,6 +15,7 @@ function App() {
           <Route path="/form" element={<Form />} />
         </Routes>
       </div>
+    </Router>
   );
 }
 
